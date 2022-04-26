b33673b2634c5730bc0016044cb377f9e202573e
death-lok <101309469+death-lok@users.noreply.github.com>
2022-04-26 16:13:46 +0530

    add drum files

---
+++ index.js
@@ -0,0 +1,78 @@
+// DETECTING BUTTON PRESS
+
+let numberOfDrums = document.querySelectorAll(".drum").length;
+
+for(let i=0; i<numberOfDrums; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        let buttonInnerHtml = this.innerHTML ;
+            console.log(buttonInnerHtml);
+
+            makeSound(buttonInnerHtml);
+            buttonAnimation(buttonInnerHtml);
+    })
+}
+
+// DETECTING KEYBOARD PRESS
+
+document.addEventListener("keypress",function(event){
+    let keys = event.key;
+
+    makeSound(keys);
+    buttonAnimation(keys);
+});
+
+// FUNCTIONS
+
+
+function makeSound(key){
+
+    switch (key){
+
+        case "w":
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            let tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            let tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "d":
+            let tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j":
+            let snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "k":
+            let crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "l":
+            let kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+            break;
+
+            default: 
+                console.log(key);
+    }
+}
+
+function buttonAnimation(currentKey){
+    let activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")
+    }, 100);
+}
